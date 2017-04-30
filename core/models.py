8c90c7e7b861b63c02403ba96af608c082605022
Sergio Delgado Quintero <sdelquin@gmail.com>
2017-04-30 12:53:21 +0100

    Added EducationLevel model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class EducationLevel(models.Model):
+    code = models.CharField(max_length=32)
+    law = models.CharField(max_length=32)
+    description = models.CharField(max_length=256)
+    enrollment_date = models.DateField()
+
+    def __str__(self):
+        return self.code
