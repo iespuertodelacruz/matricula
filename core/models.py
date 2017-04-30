531aeeb8f68def15a22e2b69c45af95da3f35967
Sergio Delgado Quintero <sdelquin@gmail.com>
2017-04-30 13:28:19 +0100

    Added order attribute to education level

--- core/models.py
+++ core/models.py
@@ -6,6 +6,12 @@ class EducationLevel(models.Model):
     law = models.CharField(max_length=32)
     description = models.CharField(max_length=256)
     enrollment_date = models.DateField()
+    order = models.SmallIntegerField()
 
     def __str__(self):
         return self.code
+
+    class Meta:
+        verbose_name = "Nivel educativo"
+        verbose_name_plural = "Niveles educativos"
+        ordering = ["order"]
