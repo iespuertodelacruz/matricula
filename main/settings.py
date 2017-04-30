c1c926c4ebb34a58cffd591787d0f3bcf7959579
Sergio Delgado Quintero <sdelquin@gmail.com>
2017-04-30 14:08:34 +0100

    Added crispy forms

--- main/settings.py
+++ main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
     "common",
     "core"
 ]
@@ -124,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = "/static/"
+
+CRISPY_TEMPLATE_PACK = "bootstrap3"
