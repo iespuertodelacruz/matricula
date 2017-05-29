e07b4650925f6cc202468304ae9a5b10afd674ce
Sergio Delgado Quintero <sdelquin@gmail.com>
2017-05-29 18:56:23 +0100

    Added cookies law

--- main/settings.py
+++ main/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "crispy_forms",
     "common",
-    "core"
+    "core",
+    "cookielaw"
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.core.context_processors.request"
             ],
         },
     },
