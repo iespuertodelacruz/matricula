65c90fa64dfe2ae8425090fde8029336c9f62719
Sergio Delgado Quintero <sdelquin@gmail.com>
2019-06-23 18:58:10 +0100

    Add migration of models to fabric

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,16 @@
 from fabric.api import env, local, prefix, cd, run
 
-env.hosts = ["matricula.iespuertodelacruz.es"]
+env.hosts = ['matricula.iespuertodelacruz.es']
 
 
 def deploy():
-    local("git push")
-    with prefix("source ~/.virtualenvs/matricula/bin/activate"):
-        with cd("~/matricula"):
-            run("git pull")
-            run("pip install -r requirements.txt")
-            run("npm install")
-            run("gulp dist")
-            run("python manage.py collectstatic --noinput")
-            run("supervisorctl restart matricula")
+    local('git push')
+    with prefix('source ~/.virtualenvs/matricula/bin/activate'):
+        with cd('~/matricula'):
+            run('git pull')
+            run('pip install -r requirements.txt')
+            run('python manage.py migrate')
+            run('npm install')
+            run('gulp dist')
+            run('python manage.py collectstatic --noinput')
+            run('supervisorctl restart matricula')
